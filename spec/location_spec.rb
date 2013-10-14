0d4f6fb9f30b77486884f6f3f554566edea55b8b
Zeljko Loncar <zeljko.loncar@onthebeach.co.uk>
2013-10-14 15:47:53 +0100

    Add wildcard tests and group under context

--- spec/location_spec.rb
+++ spec/location_spec.rb
@@ -7,7 +7,7 @@ describe Location do
   let(:other_file) { "bar.feature" }
   let(:other_line_no) { 99 }
 
-  context 'matching other locations' do
+  context 'a precise location' do
     it 'does not match a different location' do
       other = Location.new(other_file, other_line_no)
 
@@ -30,10 +30,42 @@ describe Location do
       other = WildcardLocation.new( file )
       expect( location.match?(other) ).to be_true
     end
+  end
 
-    it 'wildcard location matches location' do
+  context 'a wildcard location' do
+    it 'matches a precise location' do
       wildcard = WildcardLocation.new( file )
       expect( wildcard.match?( location ) ).to be_true
+    end
+
+    it 'does not match a precise location in a different file' do
+      wildcard = WildcardLocation.new( file )
+      location = Location.new(other_file, 10 )
+      expect( wildcard.match?( location ) ).to be_false
+    end
+
+    it 'matches a ranged location in same file' do
+      wildcard = WildcardLocation.new(file)
+      other    = RangedLocation.new(file, 3..5)
+      expect( wildcard.match?( other ) ).to be_true
+    end
+
+    it 'does not match a ranged location in a different file' do
+      wildcard = WildcardLocation.new(file)
+      other    = RangedLocation.new(other_file, 3..5)
+      expect( wildcard.match?( other ) ).to be_false
+    end
+
+    it 'matches a wildcard location' do
+      wildcard = WildcardLocation.new(file)
+      other    = WildcardLocation.new(file)
+      expect( wildcard.match?( other ) ).to be_true
+    end
+
+    it 'does not match a wildcard location in a different file' do
+      wildcard = WildcardLocation.new(file)
+      other    = WildcardLocation.new(other_file)
+      expect( wildcard.match?( other ) ).to be_false
     end
   end
 
